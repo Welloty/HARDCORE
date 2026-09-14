463c2525b06a8fc9fb44d2ad2f60cec803ea1cb1
Melolr0Focus <welloty403@gmail.com>
2026-09-14 09:41:45 +0300

    test
    When the "Exterminating" difficulty is selected, it is automatically replaced with "Impossible."
    License updated.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
-// scripts/main.js
-
-Events.on(WorldLoadEvent, () => {
-    Vars.state.rules.modeName = "Impossible";
+Events.on(WorldLoadEvent, event => {
+    if (Vars.state.rules.modeName == "eradication") {
+        Vars.state.rules.unitDamageMultiplier *= 3.0;
+        Vars.state.rules.unitHealthMultiplier *= 5.0;
+        Call.setRules(Vars.state.rules);
+    }
 });
